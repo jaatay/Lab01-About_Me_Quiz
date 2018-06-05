3d507efd23f4d32519db041d5de76ba2d45f11eb
Jesse Atay <jesse.atay@gmail.com>
2018-06-05 11:39:40 -0700

    Program works, readme completed

---
+++ About_Me_Quiz/About_Me_Quiz/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace About_Me_Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sequential method calls to run application
+            WelcomeMessage();
+            Question1();
+            Question2();
+            Question3Check(Question3());
+            Question4Check(Question4());
+            Question5();
+            FarewellMessage();
+        }
+
+        //initializes global score counter
+        static int ScoreCounter = 0;
+
+        //prints welcome message and directions to console
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Hello World! Welcome to this quiz.");
+            Console.WriteLine("You will be asked 5 questions about me. Try to guess correctly!");
+        }
+
+        //asks the user whether or not they are ready to play with boolean return
+        static bool Question1()
+        {
+            bool Ready = false;
+
+            while (Ready == false)
+            {
+                Console.WriteLine("Are you ready? Type y/yes when ready.");
+                string UserInput = Console.ReadLine();
+
+                if (UserInput == "yes" || UserInput == "y")
+                {
+                    Console.WriteLine("Congratulations! One down, four to go!");
+                    Ready = true;
+                }
+            }
+
+            ScoreCounter++;
+
+            return true;
+        }
+
+        //asks user second question
+        static void Question2()
+        {
+            Console.WriteLine("Next question!");
+            Console.WriteLine("Semper Fi? Type y/yes or n/no.");
+            string UserInput = Console.ReadLine();
+
+            if (UserInput == "yes" || UserInput == "y")
+            {
+                Console.WriteLine("Oorah!");
+                ScoreCounter++;
+            } else if (UserInput == "no" || UserInput == "n")
+            {
+                Console.WriteLine("Meh.");
+            }
+        }
+
+        //asks user third question, parses string to return an integer as a response, used in the question check below
+        static int Question3()
+        {
+            Console.WriteLine("Next question!");
+            Console.WriteLine("What is my favorite number?");
+            string UserInput = Console.ReadLine();
+            return int.Parse(UserInput);
+        }
+
+        //method using user answer from question 3 as argument
+        static void Question3Check(int UserNumber)
+        {
+            if (UserNumber == 42)
+            {
+                Console.WriteLine("That is correct. Truly you understand life, love, and everything.");
+                ScoreCounter++;
+            } else
+            {
+                Console.WriteLine("Unfortunately that is incorrect. In another dimension, you just entered the nuclear launch codes.");
+                Console.WriteLine("The correct answer was: 42");
+            }
+        }
+
+        //asks user fourth question, returns typed response as a string
+        static string Question4()
+        {
+            Console.WriteLine("Next question!");
+            Console.WriteLine("Besides Oregon, name the first State I lived in. (Type your answer)");
+            string UserInput = Console.ReadLine();
+
+            return UserInput;
+        }
+
+        //method to check user's answer to question 4, accepting the previous string as an argument
+        static void Question4Check(string UserAnswer)
+        {
+            if (UserAnswer == "liquid" || UserAnswer == "Liquid")
+            {
+                Console.WriteLine("Wow. Just...wow. Here's a point!");
+                ScoreCounter++;
+            } else
+            {
+                Console.WriteLine("Nice Try, the correct answer was: liquid");
+            }
+        }
+        
+        //asks user question 5, no return, evaluates string response
+        static void Question5()
+        {
+            Console.WriteLine("Last question!");
+            Console.WriteLine("Have I been to Syria?(type y/yes or n/no to answer.)");
+            string UserInput = Console.ReadLine();
+
+            if (UserInput == "y" || UserInput == "yes")
+            {
+                Console.WriteLine("Yes, I have been to Syria on accident.");
+                ScoreCounter++;
+            }
+            else
+            {
+                Console.WriteLine("That is incorrect. I have been to Syria.");
+            }
+        }
+
+        //farewell message to thank the user for playing and calculate final score
+        static void FarewellMessage()
+        {
+            Console.Write("Thanks for playing!");
+            Console.Write($"Your score is {ScoreCounter} out of 5. Press any key to exit.");
+            Console.ReadLine();
+        }
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lab01-About_Me_Quiz
+
+## About this project
+This program asks the user to play a game. The goal is to try and guess the correct answers. No animals were harmed in the making of this program.
+
+## How to use
+This program requires Visual Studio. Once the repo has been cloned to the user's local machine, they simply need to open the solution using Visual Studio and run the program. Afer that, simply follow the prompts and enjoy.
